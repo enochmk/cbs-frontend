8dccadb2735abd7ce8dfc2bebbb007c590910de9
Enoch Klufio <enochmk94@gmail.com>
2022-06-16 15:13:21 +0000

    refactor: serve public directory

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,7 @@ const env: string = config.get('env');
 app.use(helmet());
 app.use(cors());
 app.use(morgan('dev'));
+app.use('/public', express.static('public'));
 app.use(express.json());
 app.use(handleRequestID);
 app.use('/api/batch', batchRoutes);
@@ -25,7 +26,7 @@ app.use(errorHandler);
 //starting server
 try {
 	app.listen(port, async () => {
-		const message = `Server is running on localhost:${port}`;
+		const message = `Server is running on localhost:${port} in ${env} mode`;
 		logger.info(message);
 	});
 } catch (error: any) {
