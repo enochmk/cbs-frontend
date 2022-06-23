11981355e6eb319a71b96518d813f352f7aab74e
Enoch Klufio <enochmk94@gmail.com>
2022-06-23 11:23:15 +0000

    refactor: adjust account page

--- src/pages/AdjustAccount.tsx
+++ src/pages/AdjustAccount.tsx
@@ -1,19 +1,20 @@
 function AdjustAccount() {
   return (
-    <section className="flex flex-wrap justify-center -m-24">
-      <div className="card w-1/2 bg-base-100 shadow-xl">
-        <form action="#">
-          <div className="card-body">
-            <h2 className="card-title uppercase text-center">Adjust Account</h2>
-            <hr />
-            <label className="block mb-2  font-medium " htmlFor="file_input">Choose a file with content format: MSISDN|ACCOUNT TYPE|REMARK</label>
-            <input className="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none" id="file_input" type="file" />
-
-            <div className="card-actions justify-center">
-              <button className="btn btn-primary btn-block">Upload</button>
-            </div>
-          </div>
-        </form>
+    <section className="flex flex-wrap justify-center mt-24">
+      <div className="block rounded-lg shadow-lg bg-white w-1/3">
+        <h2 className="py-3 px-6 border-b border-gray-300 font-extrabold uppercase text-lg text-gray-700 text-center">
+          Adjust Account
+        </h2>
+        <div className="p-6">
+          <label className="block mb-2" htmlFor="file_input">Choose a file</label>
+          <input className="block p-5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none" id="file_input" type="file" />
+        </div>
+        <div className="py-3 px-6 border-t border-gray-300 text-gray-600">
+          <button className="btn btn-primary btn-block gap-2">
+            <i className="fa-solid fa-upload" />
+            Upload
+          </button>
+        </div>
       </div>
     </section>
   );
