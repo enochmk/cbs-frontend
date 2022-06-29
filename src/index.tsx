56ab227d74dd76a99604654d80e80b068365ca95
Enoch Klufio <enochmk94@gmail.com>
2022-06-29 12:31:44 +0000

    refactor: configure redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/css/index.css';
 import './assets/css/tailwind.css';
-
+import store from './app/store';
 import App from './App';
 
 const rootDev = document.getElementById('root')!;
@@ -13,8 +14,10 @@ const root = ReactDOM.createRoot(rootDev);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
