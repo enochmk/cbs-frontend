f4b79bd10eaa1b090f0b319e513d872735b00f19
Enoch Klufio <enochmk94@gmail.com>
2022-06-30 10:55:55 +0000

    refactor: set axios timeout to 30 mins

--- src/customs/axios.ts
+++ src/customs/axios.ts
@@ -4,13 +4,13 @@ const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 30 * 60000,
   withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 30 * 60000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
